704b65b6498a2b9eb3a565d3bc7c36c71611c405
robertatli <robbster10@gmail.com>
2016-11-18 10:32:07 +0000

    Basic layout vefsíðu

--- weblinks/index.php
+++ weblinks/index.php
@@ -2,12 +2,17 @@
 <html lang="">
 <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="./css/stilsida.css">
+	<link rel="stylesheet" href="./css/normalize.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
 </head>
 
 <body>
-    <p>Halló</p>
-    <p>Test</p>
+	<div class="scroll">
+	<div class="in-scroll">
+		<h2>Welcome people</h2>
+	</div>
+	</div>
 </body>
 </html>
