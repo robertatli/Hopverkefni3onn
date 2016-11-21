45a2af5c03d16092b908f17e4683762fd1d303a8
robertatli <robbster10@gmail.com>
2016-11-21 14:30:28 +0000

    Kafli1 kominn

--- weblinks/IndexFolder/Sida1.php
+++ weblinks/IndexFolder/Sida1.php
@@ -15,10 +15,13 @@
 			<header>
 				<h3>Prologue</h3>
 			</header>
-				Once there was a mighty King that ruled the lands of Garonril. He was a great King and fought to defend his land from the horroring invaders. He fought so well that all who faced him fell by his might with the sword, untill he was faced by a young girl named Saari. The girl possesed magical powers unmatched by any who faced her.The King fought valiantly and inflicted many deep wounds. The girl was shocked by The Kings might, She had never faced such a worthy foe. She fought with all her power untill she managed to knock The Kings sword from him. The King knew that he was powerless without the sword.  The king fell by her hand and so his kingdom too fell into ruins and his people forced into slavery by The New Queen.
-				
+				Once there was a mighty King that ruled the lands of Garonril. He was a great King and fought to defend his land from the horroring invaders. He fought so well that all who faced him fell by his mighty sword Sundri, untill he was faced by a young girl named Saari. The girl possesed magical powers unmatched by any who faced her.The King fought valiantly and inflicted many deep wounds. The girl was shocked by The Kings might, She had never faced such a worthy foe. She fought with all her power untill she managed to knock The Kings sword from him. The King knew that he was powerless without the sword.  The king fell by her hand and so his kingdom too fell into ruins and his people forced into slavery by The New Queen. The Queen told one of her followers to take The Kings sword far away from the castle and guard it with his life. And so he did, Now all that is left of the sword are wispers in the wind of where it might be hidden.<br><br>
+				Your story starts in a quiet Inn in the center of a small village a few miles away from the Evil Queens Castle.
 			</div>
-			<form action="./sida2.php">
+			<br><br><br>
+			<br><br><br>
+			<br><br><br>
+			<form action="./SidaSagabyrjar.php">
     			<strong><input type="submit" value="Begin" class="button" /></strong>
 			</form>
 		</div>			
---
+++ weblinks/IndexFolder/SidaSagabyrjar.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../css/stilsida.css">
+	<link rel="stylesheet" href="../css/normalize.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+
+<body>
+	<div class="scroll">
+		<div class="wrapper2">
+			<div class="textmanager">
+			<header>
+				<h3>Chapter 1.</h3>
+				<span class="h4">The Inn</span>
+			</header>
+			Two men walk into the Inn. You overhear them talking about The Queen and you become intrested in their conversation. You lean in closer to hear what they are saying. One of the men is talking about a rumor he heard about the sword and where it might be located. You hear him mention a cave on the east side of the castle. The other man interupts him saying that he heard it was placed in the ground and a tree was made grow around it. You take another sip of your beverage. They start to talk about something else and you stop listening. You think to yourself that that sword would be worth a fortune if anyone could find it. You finish your drink and head to your room in the Inn.
+			As you lay down in your bed you quickly fall asleep dreaming of the fortune that awaits you. <br><br> The next day you head out and decide to go explore the surroundings.
+				
+			</div>
+			s
+		</div>			
+	</div>
+</body>
+</html>
