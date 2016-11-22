7da0e0fe480ca1ecb914e66ebcd5b1469119ebbd
robertatli <robbster10@gmail.com>
2016-11-22 14:32:56 +0000

    Smá laganir

--- weblinks/IndexFolder/SidaSagabyrjar.php
+++ weblinks/IndexFolder/SidaSagabyrjar.php
@@ -16,9 +16,18 @@
 				<span class="h4">The Inn</span>
 			</header>
 			Two men walk into the Inn. You overhear them talking about The Queen and you become intrested in their conversation. You lean in closer to hear what they are saying. One of the men is talking about a rumor he heard about the sword and where it might be located. You hear him mention a cave on the east side of the castle. The other man interupts him saying that he heard it was placed in the ground and a tree was made grow around it. You take another sip of your beverage. They start to talk about something else and you stop listening. You think to yourself that that sword would be worth a fortune if anyone could find it. You finish your drink and head to your room in the Inn.
-			As you lay down in your bed you quickly fall asleep dreaming of the fortune that awaits you. <br><br> The next day you head out and decide to go explore the surroundings.
+			As you lay down in your bed you quickly fall asleep dreaming of the fortune that awaits you. <br><br> The next day you gather your things and head out and decide to go explore the surroundings. You see the two men are outside. You approach them and ask them about the...
+			<br><br><br>
+			<form action="./chapter1.2-2.php" style="float:left;padding-left:27%;">
+    			<strong><input type="submit" value="Cave" class="button" /></strong>
+			</form>
+			<form action="./chapter1.2.php" style=" float:right;padding-right:27%;">
+    			<strong><input type="submit" value="Tree" class="button" /></strong>
+			</form>
 				
 			</div>
+			
+			
 		</div>			
 	</div>
 </body>
