500fc057eabb15771000d95f9ef9da9ce334a276
robertatli <robbster10@gmail.com>
2016-11-28 19:24:49 +0000

    bætti við meira dialogue hja willie

--- weblinks/IndexFolder/Chapter1.3[lake].php
+++ weblinks/IndexFolder/Chapter1.3[lake].php
@@ -14,7 +14,7 @@
 			<div class="textmanager">
 			<header>
 				<h3>Chapter 1.3</h3>
-				<span class="h4">The lake</span>
+				<span class="h4">The Fisherman</span>
 			</header>
 				<div class="Book">
 					<br>
@@ -25,25 +25,66 @@
 						<div class="speech">
 						"Hello my name's Willy!, What can I do for you?"
 						</div><br><br><br>
-						<a href="#001">Ask him about the cave</a><br><br><a href="./chapter1.2[cave].php">Go back to the crossroads</a>
+						<a href="#001" class="speech2">Ask him about the cave</a><br><br><a href="./chapter1.2[cave].php" class="speech2">Go back to the crossroads</a><br><br>
 					</section>
+
+
+
 					<section id="001">
 					<br><img src="../pixels/characters/willy.png" style="width:200px;height: 200px;"><br>
 						<div class="speech">
 						"Why yes! There is a cave in a large hill close to here. That might be the one you are looking for."
 						</div><br><br><br>
-						<a href="#002">Ask him if he knows a quick way to the cave</a><br><br><a href="./chapter1.2[cave].php">Go back to the crossroads</a>
+						<a href="#002" class="speech2">Ask him if he knows a quick way to the cave</a><br><br><a href="./chapter1.2[cave].php" class="speech2">Go back to the crossroads</a><br><br>
 					</section>
+
+
+
 					<section id="002">
 					<br><img src="../pixels/characters/willy.png" style="width:200px;height: 200px;"><br>
 						<div class="speech" id="002">
 						"Yes! there is a quick way to get to the cave, You just have to go throught these here woods." He points out a small road through some trees.
 						</div><br><br><br>
-						<a href="./chapter1.4[shortcut].php">Thank him and head to the small road</a><br><br><a href="./chapter1.2[cave].php">Thank him and go back to the crossroads</a>
+						<a href="./chapter1.4[shortcut].php" class="speech2">Thank him and head to the small road</a><br><br><a href="./chapter1.2[cave].php" class="speech2">Thank him and go back to the crossroads</a><br><br>
+						<a href="#003" class="speech2">Ask him if he has any fish.</a>
 					</section>
+
+
+
+					<section id="003">
+					<br><img src="../pixels/characters/willy.png" style="width:200px;height: 200px;"><br>
+						<div class="speech" id="002">
+						"Yes! I have plenty of fish. Would you like to buy some?"
+						</div><br><br><br>
+						<a href="#004" class="speech2">Buy some fish</a><br><br><a href="#005" class="speech2">Dont buy fish</a><br><br>
+					</section>
+
+
+
+					<section id="004">You buy some fish
+					<br><img src="../pixels/characters/willy.png" style="width:200px;height: 200px;"><br>
+						<div class="speech" id="002">
+						"Here you Go, will that be all?"
+						</div><br><br><br>
+						<a href="./chapter1.4[shortcut].php" class="speech2">Thank him and head to the small road</a><br><br><a href="./chapter1.2[cave].php" class="speech2">Go back to the crossroads</a><br><br>
+
+					</section>
+
+
+
+					<section id="005">You dont buy fish
+					<br><img src="../pixels/characters/willy.png" style="width:200px;height: 200px;"><br>
+						<div class="speech" id="002">
+						"Good luck on your journey!"
+						</div><br><br><br>
+						<a href="./chapter1.4[shortcut].php" class="speech2">Thank him and head to the small road</a><br><br><a href="./chapter1.2[cave].php" class="speech2">Go back to the crossroads</a><br><br>
+					</section>
+
+
+
 					<section class="intro">By the lake you see a man fishing.
 					<br><br><br><br><br><br>
-						<a href="#000">Talk to him</a><br><br><a href="./chapter1.2[cave].php">Go back to the crossroads</a>
+						<a href="#000" class="speech2">Talk to him</a><br><br><a href="./chapter1.2[cave].php" class="speech2">Go back to the crossroads</a>
 					</section>
 				</div>			
 			</div>
