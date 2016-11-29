71fca914941431df0fa74cc1b82a7dd3a126763b
FunkyMicrowave <snefill@gmail.com>
2016-11-29 11:30:47 +0000

    veit ekki lengur hvað ég gerði

---
+++ weblinks/IndexFolder/mobile.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <style>
+        body{
+            text-align: center; 
+            height: 100vh;
+            width: 100vw;
+            overflow: hidden;
+            margin: 0;
+            padding: 0;
+        }
+        img{
+            height: 30%;
+            width: auto;
+        }
+        h1{
+            margin-top: 40%;
+            height: 10em;
+            font-size: 1.5em;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>There is not a mobile version yet but we are working on it <img src="../pixels/loading.gif"></h1>
+</body>
+</html>
--- weblinks/auka/character.php
+++ weblinks/auka/character.php
@@ -3,7 +3,7 @@ if(isset($_GET['id'])){
     $_SESSION['id'] = $_GET['id'];
 }
  ?>
-<div class="character-set" style="height: 12em;width: 20em;margin-top:2em;margin-left:2em;margin-bottom:-16em;background: rgba(90,90,90,.4);border-radius:25px;">
+<div class="character-set" style="height: 12em;width: 90%;margin-top:2em;margin-left:2em;margin-bottom:-14em;background: rgba(90,90,90,.4);border-radius:25px;">
             <img style="height:100%;width:auto;" class="character-set" src="<?php if($_SESSION['id'] === 'male'){?>../pixels/big/GenericMaleLarge.png<?php }else if($_SESSION['id'] === 'female'){?>../pixels/big/GenericFemaleLarge.png<?php }else{
     
 header('Location: ../');} ?>">
