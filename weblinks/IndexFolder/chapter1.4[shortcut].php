1b2eab71ffad32c6003fb1263b6b3d035a68de01
robertatli <robbster10@gmail.com>
2016-11-29 14:50:13 +0000

    bætti við Shortcut siðunni,myndir fyrir hana.

---
+++ weblinks/IndexFolder/chapter1.4[shortcut].php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../css/stilsida.css">
+	<link rel="stylesheet" href="../css/normalize.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+
+<body>
+	<div class="scroll">
+        <?php include("../auka/character.php"); ?>
+		<div class="wrapper2">
+			<div class="textmanager">
+			<header>
+				<h3>Chapter 1.4</h3>
+				<span class="h4">The Shortcut?</span>
+			</header>
+				You follow the small road and after some time find yourself outside a forest. You think to yourself that the road didnt lead to the hill, but it continued into the forest.<br><br> You can follow the road into the forest or go back to the crossroads.
+			</div><br><br>
+			<div class="Gender">
+			<a href="./Chapter1.2[cave].php">
+					<img src="../pixels/crossroads.png" onmouseover="style='filter: gray;filter: grayscale(1);webkit-filter: grayscale(1);'" onmouseout="style='filter: none;'">
+				</a>
+				<a href="./Chapter1.3[forest].php">
+					<img src="../pixels/skogur.png" onmouseover="style='filter: gray;filter: grayscale(1);webkit-filter: grayscale(1);'" onmouseout="style='filter: none;'">
+				</a>
+			</div>
+			
+		</div>			
+	</div>
+</body>
+</html>
