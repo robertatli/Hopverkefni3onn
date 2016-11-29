57819f731d07484367959e1ca1e6e5af6adbe0cd
FunkyMicrowave <snefill@gmail.com>
2016-11-29 09:14:13 +0000

    breytti fyrir síma og bætti við tré og helli

--- weblinks/IndexFolder/Chapter1.1.php
+++ weblinks/IndexFolder/Chapter1.1.php
@@ -13,18 +13,20 @@
 		<div class="wrapper2">
 			<div class="textmanager">
 			<header>
-				<h3>Chapter 1.</h3>
+				<h3>Chapter 1.1</h3>
 				<span class="h4">The Inn</span>
 			</header>
 			Two men walk into the Inn. You overhear them talking about The Queen and you become intrested in their conversation. You lean in closer to hear what they are saying. One of the men is talking about a rumor he heard about the sword and where it might be located. You hear him mention a cave on the east side of the castle. The other man interupts him saying that he heard it was placed in the ground and a tree was made grow around it. You take another sip of your beverage. They start to talk about something else and you stop listening. You think to yourself that that sword would be worth a fortune if anyone could find it. You finish your drink and head to your room in the Inn.
 			As you lay down in your bed you quickly fall asleep dreaming of the fortune that awaits you. <br><br> The next day you gather your things and head out and decide to go explore the surroundings. You see the two men are outside. You approach them and ask them about the...
 			<br><br><br>
-			<form action="./chapter1.2[cave].php" style="float:left;padding-left:27%;">
-    			<strong><input type="submit" value="Cave" class="button" onmouseover="style='filter: gray;filter: grayscale(1);webkit-filter: grayscale(1);'" onmouseout="style='filter: none;'"/></strong>
-			</form>
-			<form action="./chapter1.2[tre].php" style=" float:right;padding-right:27%;">
-    			<strong><input type="submit" value="Tree" class="button" onmouseover="style='filter: gray;filter: grayscale(1);webkit-filter: grayscale(1);'" onmouseout="style='filter: none;'" /></strong>
-			</form>
+                <div class="Gender">
+				<a href="./chapter1.2[cave].php">
+					<img src="../pixels/big/Cave.png" onmouseover="style='filter: gray;filter: grayscale(1);webkit-filter: grayscale(1);'" onmouseout="style='filter: none;'">
+				</a>
+				<a href="./chapter1.2[tre].php">
+					<img src="../pixels/big/Tree.png" onmouseover="style='filter: gray;filter: grayscale(1);webkit-filter: grayscale(1);'" onmouseout="style='filter: none;'">
+				</a>
+				</div>
 				
 			</div>
 			
