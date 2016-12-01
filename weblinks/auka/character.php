b0ab9f1c195707b5c9885f211088d24e694896bf
FunkyMicrowave <snefill@gmail.com>
2016-12-01 08:39:59 +0000

    breytti character hudinu

--- weblinks/auka/character.php
+++ weblinks/auka/character.php
@@ -3,9 +3,31 @@ if(isset($_GET['id'])){
     $_SESSION['id'] = $_GET['id'];
 }
  ?>
-<div class="character-set" style="height: 12em;width: 90%;margin-top:2em;margin-left:2em;margin-bottom:-14em;background: rgba(90,90,90,.4);border-radius:25px;">
-            <img style="height:100%;width:auto;" class="character-set" src="<?php if($_SESSION['id'] === 'male'){?>../pixels/big/GenericMaleLarge.png<?php }else if($_SESSION['id'] === 'female'){?>../pixels/big/GenericFemaleLarge.png<?php }else{
+<style>
+    body{
+        overflow: hidden;
+    }
+    .character-set{
+        position:fixed;
+        bottom:14em;
+        left: 25%;
+        z-index:4;
+        height:6em;
+        width: 960px;
+        margin-top:2em;
+        margin-left:2em;
+        margin-bottom:-14em;
+        background: rgba(90,90,90,1);
+        border-radius:25px;
+        border: 10px solid transparent;
+        -webkit-border-image: url(http://i36.tinypic.com/4tkqhi.jpg) 20% round;
+        -o-border-image: url(http://i36.tinypic.com/4tkqhi.jpg) 20% round;
+        border-image: url(http://i36.tinypic.com/4tkqhi.jpg) 20% round;
+    }
+</style>
+<div class="character-set" style="">
+            <img style="height:100%;width:auto;" src="<?php if($_SESSION['id'] === 'male'){?>../pixels/big/GenericMaleLarge.png<?php }else if($_SESSION['id'] === 'female'){?>../pixels/big/GenericFemaleLarge.png<?php }else{
     
 header('Location: ../');} ?>">
-            <img style="height:50%;width:auto;padding-bottom:2em;" class="character-set" src="../pixels/Weapons/stick.png">
+            <img style="height:50%;width:auto;padding-bottom:2em;" src="../pixels/Weapons/stick.png">
         </div>
